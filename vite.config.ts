25b919ec994f8a6f19e9bf58237354a58cb85be0
mejiyinka-web <mejiyinka@gmail.com>
2026-06-05 00:38:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,7 @@ export default defineConfig(({ mode }) => ({
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
 }));
+export default defineConfig({
+  base: '/build-my-showcase-75-9f1fec41/',
+  plugins: [react()],
+})
